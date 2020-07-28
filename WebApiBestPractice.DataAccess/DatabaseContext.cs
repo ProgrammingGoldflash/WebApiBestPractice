7c5d7513fd0e27dc0a53744cbc8cddd796e737b4
Philip Ainberger <p.ainberger@countit.at>
2020-07-28 20:57:27 +0200

    created DB-Context

---
+++ WebApiBestPractice.DataAccess/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiBestPractice.DataAccess.Entities;
+
+namespace WebApiBestPractice.DataAccess
+{
+    public class DatabaseContext : DbContext
+    {
+        #region Public Constructors
+
+        public DatabaseContext() : base()
+        {
+        }
+
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        #endregion Public Properties
+    }
+}
