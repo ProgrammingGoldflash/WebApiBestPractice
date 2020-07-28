87fca6d5e81ecdc647f5fc24d3a884219d9bc879
Philip Ainberger <p.ainberger@countit.at>
2020-07-28 20:57:05 +0200

    added entities

--- WebApiBestPractice.DataAccess/Entities/Customer.cs
+++ WebApiBestPractice.DataAccess/Entities/Customer.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApiBestPractice.DataAccess.Entities
 {
     public class Customer
     {
+        #region Public Properties
+
+        public string Email { get; set; }
+
+        public string FirstName { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        public User User { get; set; }
+
+        public int UserId { get; set; }
+
+        #endregion Public Properties
     }
 }
--- WebApiBestPractice.DataAccess/Entities/User.cs
+++ WebApiBestPractice.DataAccess/Entities/User.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApiBestPractice.DataAccess.Entities
 {
     public class User
     {
+        #region Public Properties
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        #endregion Public Properties
     }
 }
